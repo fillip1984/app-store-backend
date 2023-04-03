63b12a8ec819ad3676605439d14651ab03db8ca5
fillip1984 <fillip1984@gmail.com>
2023-04-02 21:13:16 -0400

    initial commit

---
+++ src/main/java/org/home/knowledge/appstore/AppStoreApplication.java
@@ -0,0 +1,13 @@
+package org.home.knowledge.appstore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppStoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppStoreApplication.class, args);
+	}
+
+}
