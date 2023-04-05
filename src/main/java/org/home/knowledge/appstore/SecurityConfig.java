2c71f64f16a86ea8bdbf5b278a8f859923d372fd
fillip1984 <fillip1984@gmail.com>
2023-04-04 20:37:05 -0400

    working out security

--- src/main/java/org/home/knowledge/appstore/SecurityConfig.java
+++ src/main/java/org/home/knowledge/appstore/SecurityConfig.java
@@ -1,5 +1,7 @@
 package org.home.knowledge.appstore;
 
+import static org.springframework.security.config.Customizer.withDefaults;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -22,45 +24,49 @@ import lombok.extern.slf4j.Slf4j;
 public class SecurityConfig {
 
     @Bean
-    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+    SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         log.warn(
                 "H2 database console is unsecured for development purposes. THIS CONFIGURATION SHOULD NOT MAKE IT INTO A QA OR PROD ENVIRONMENT!!!");
         // @formatter:off
-        // See: https://www.baeldung.com/spring-cors
-        // H2 console required frameOptions be disabled, see: https://springframework.guru/using-the-h2-database-console-in-spring-boot-with-spring-security/
-        http.cors()
-            .and()
-            .authorizeRequests()
-            .antMatchers("/", "/status", "/v3/api-docs/**").permitAll()
-            .antMatchers("/h2-console/**", "/swagger-ui/**").hasAnyRole("USER", "ADMIN")
-            .antMatchers("/actuator/**", "/admin/**").hasAnyRole("ADMIN")
-            .anyRequest().authenticated()
-            .and()
-            .httpBasic()
-            .and()
-            .csrf().disable()
-            .headers().frameOptions().disable()
-            .and()
-            .exceptionHandling().accessDeniedPage("/accessDenied.html");
-        return http.build();
+        return http
+                .csrf(csrf -> csrf.disable())
+                .authorizeHttpRequests(auth -> {
+                    auth.antMatchers("/", "/status", "/v3/api-docs/**").permitAll();
+                    auth.antMatchers("/h2-console/**", "/swagger-ui/**").hasAnyRole("USER", "ADMIN");
+                    auth.antMatchers("/actuator/**", "/admin/**").hasAnyRole("ADMIN");
+                    auth.anyRequest().authenticated();
+                })
+                .httpBasic(withDefaults())
+                // See: https://www.baeldung.com/spring-cors
+                // H2 console required frameOptions be disabled, see: https://springframework.guru/using-the-h2-database-console-in-spring-boot-with-spring-security/
+                .cors(withDefaults())
+                .headers(headers -> {
+                    headers.frameOptions(frameOptions -> {
+                        frameOptions.disable();
+                    });
+                })
+                .exceptionHandling(exceptionHandling -> {
+                    exceptionHandling.accessDeniedPage("/accessDenied.html");
+                })
+                .build();
         // @formatter:on
     }
 
     @Bean
-    public InMemoryUserDetailsManager inMemoryUserDetailsManager() {
+    InMemoryUserDetailsManager inMemoryUserDetailsManager() {
         log.warn(
                 "NoOp password encoder is being used with in memory authentication. THIS CONFIGURATION SHOULD NOT MAKE IT INTO A QA OR PROD ENVIRONMENT!!!");
         // @formatter:off
-            var user = User.builder()
-                            .username("user")
-                            .password("{noop}user")
-                            .roles("USER")
-                            .build();
-            var admin = User.builder()
-                            .username("admin")
-                            .password("{noop}admin")
-                            .roles("USER", "ADMIN")
-                            .build();
+        var user = User.builder()
+                        .username("user")
+                        .password("{noop}user")
+                        .roles("USER")
+                        .build();
+        var admin = User.builder()
+                        .username("admin")
+                        .password("{noop}admin")
+                        .roles("USER", "ADMIN")
+                        .build();
         // @formatter:on
         return new InMemoryUserDetailsManager(user, admin);
     }
