426e61e3238ea949a4ca3d0082cc958d0351bc17
fillip1984 <fillip1984@gmail.com>
2023-04-15 14:18:23 -0400

    adding tag model

--- src/main/java/org/home/knowledge/appstore/model/Tag.java
+++ src/main/java/org/home/knowledge/appstore/model/Tag.java
@@ -3,20 +3,14 @@ package org.home.knowledge.appstore.model;
 import java.util.List;
 
 import javax.persistence.Entity;
-import javax.persistence.EnumType;
-import javax.persistence.Enumerated;
 import javax.persistence.Inheritance;
 import javax.persistence.InheritanceType;
-import javax.persistence.JoinColumn;
-import javax.persistence.JoinTable;
 import javax.persistence.ManyToMany;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 
 import org.home.knowledge.appstore.model.spec.AbstractEntity;
-import org.home.knowledge.appstore.model.typed.Category;
-import org.home.knowledge.appstore.model.typed.Status;
 
 import lombok.Data;
 import lombok.EqualsAndHashCode;
@@ -25,7 +19,7 @@ import lombok.EqualsAndHashCode;
 @Inheritance(strategy = InheritanceType.TABLE_PER_CLASS)
 @Data
 @EqualsAndHashCode(callSuper = true)
-public class Application extends AbstractEntity {
+public class Tag extends AbstractEntity {
 
     @NotNull
     @NotBlank
@@ -37,16 +31,6 @@ public class Application extends AbstractEntity {
     @Size(min = 10, max = 500)
     private String description;
 
-    private String repositoryUrl;
-
-    @Enumerated(EnumType.STRING)
-    private Status status;
-
-    @Enumerated(EnumType.STRING)
-    private Category category;
-
-    @ManyToMany
-    @JoinTable(name = "APPLICATION_TAG", joinColumns = @JoinColumn(name = "application_id", referencedColumnName = "id"), inverseJoinColumns = @JoinColumn(name = "tag_id", referencedColumnName = "id"))
-    private List<Tag> tags;
-
+    @ManyToMany(mappedBy = "tags")
+    private List<Application> applications;
 }
